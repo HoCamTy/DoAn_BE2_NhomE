3588f0abe1e2a3a175160631b9dcf6d1aaa292c3
Dtkxuyen2004 <dinhthikimxuyen24@gmail.com>
2025-05-20 10:21:41 +0700

    datlaimatkhau

--- routes/web.php
+++ routes/web.php
@@ -6,26 +6,21 @@ use App\Http\Controllers\CrudUserController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\ResetPasswordController;
 use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
-// Public routes
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-Route::get('/register', [RegisterController::class, 'showRegistrationForm'])
-    ->name('register');
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
+
 // Protected admin routes
 Route::middleware(['checkauth'])->prefix('admin')->group(function () {
     Route::get('/dashboard', function () {
@@ -34,7 +29,6 @@ Route::middleware(['checkauth'])->prefix('admin')->group(function () {
 
     Route::resource('services', ServiceController::class);
     Route::resource('appointments', AppointmentController::class);
-    // Other admin routes
 });
 
 // Protected customer routes
@@ -42,15 +36,11 @@ Route::middleware(['checkauth'])->prefix('customer')->group(function () {
     Route::get('/dashboard', function () {
         return view('customer.dashboard');
     })->name('customer.dashboard');
-    Route::get('/my-appointments', [AppointmentController::class, 'myAppointments'])
-        ->name('customer.appointments');
-    Route::post('/appointments/{appointment}/cancel', [AppointmentController::class, 'cancelAppointment'])
-        ->name('customer.appointments.cancel');
-    Route::get('/appointments/create', [AppointmentController::class, 'customerCreate'])
-        ->name('customer.appointments.create');
-    Route::post('/appointments', [AppointmentController::class, 'customerStore'])
-        ->name('customer.appointments.store');
-    // Other customer routes
+
+    Route::get('/my-appointments', [AppointmentController::class, 'myAppointments'])->name('customer.appointments');
+    Route::post('/appointments/{appointment}/cancel', [AppointmentController::class, 'cancelAppointment'])->name('customer.appointments.cancel');
+    Route::get('/appointments/create', [AppointmentController::class, 'customerCreate'])->name('customer.appointments.create');
+    Route::post('/appointments', [AppointmentController::class, 'customerStore'])->name('customer.appointments.store');
 });
 
 // Home route
@@ -63,3 +53,11 @@ Route::get('/', function () {
     }
     return redirect()->route('login');
 })->name('home');
+
+// Profile routes
+Route::get('/profile', [CrudUserController::class, 'showProfile'])->name('profile');
+Route::post('/profile', [CrudUserController::class, 'updateProfile']);
+
+// Reset password routes
+Route::get('/reset-password', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('/reset-password', [ResetPasswordController::class, 'resetPassword'])->name('password.update');
